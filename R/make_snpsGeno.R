47c726f3e8fb8ee1acbeb088c9b446e677a3ab8e
joshstolz <josh.stolz@libd.org>
2021-03-12 15:15:07 -0500

    added changes to make_snpsGeno

---
+++ R/make_snpsGeno.R
@@ -0,0 +1,20 @@
+#' Title
+#'
+#' @param genotype object from variantAnnotation::readVcf
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' genotyped<-readVcf("Data/LIBD_Brain_merged_topmed_RNASeq_annotated_variants_043020.vcf","hg38")
+#' test<-make_snpsGeno(genotyped)
+make_snpsGeno<-function(genotyped){
+    snpsGeno = VariantAnnotation::geno(genotyped)$GT
+    snpsGeno[snpsGeno == "./."] = NA
+    snpsGeno[snpsGeno == "0|0"] = 0
+    snpsGeno[snpsGeno == "0|1"] = 1
+    snpsGeno[snpsGeno == "1|0"] = 1
+    snpsGeno[snpsGeno == "1|1"] = 2
+    class(snpsGeno) = "integer"
+    return(snpsGeno)
+}
