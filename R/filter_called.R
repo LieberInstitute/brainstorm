6d8193c7510818fd12f63f045753f7f28f01ff2e
joshstolz <josh.stolz@libd.org>
2021-03-19 14:57:14 -0400

    add new function for filtering called. close #1

---
+++ R/filter_called.R
@@ -0,0 +1,24 @@
+#' Title
+#'
+#' @param called object from compiling genotypes of rna_seq
+#' @param DP_min
+#' @param DP_max
+#' @param ref_call
+#' @param alt_call
+#' @param overlaps
+#' @param min_VDB
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' filter_called(genotyped)
+filter_called <- function(called,DP_min=5,DP_max=80, ref_call=1,alt_call=1, overlaps=1, min_VDB=.1){
+    called = called[VariantAnnotation::info(called)$DP > DP_min*ncol(called) &
+            VariantAnnotation::info(called)$DP < DP_max*ncol(called) &
+            Biostrings::nchar(VariantAnnotation::ref(called)) == ref_call &
+            elementNROWS(VariantAnnotation::alt(called)) == alt_call &
+            GenomicRanges::countOverlaps(called, type = "equal") == overlaps &
+            VariantAnnotation::info(called)$VDB >min_VDB,]
+    return(called)
+}
---
+++ data-raw/genotyped.R
@@ -0,0 +1,5 @@
+## code to prepare `genotyped` dataset goes here
+
+genotyped = VariantAnnotation::readVcf("Data/LIBD_Brain_merged_topmed_RNASeq_annotated_variants_043020.vcf",
+                    genome="hg38")
+usethis::use_data(genotyped, overwrite = TRUE)
