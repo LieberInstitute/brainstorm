b2a05778c62ee6feee83a8e5477ac1a6ebae22c1
joshstolz <josh.stolz@libd.org>
2021-03-19 14:59:31 -0400

    remove unneeded code

--- R/make_corLong.R
+++ R/make_corLong.R
@@ -15,15 +15,10 @@ make_corLong<- function(snpsGeno,SampleDataPath,ID_col="ID"){
     corLong$rowSample = rep(colnames(snpsGeno), times = ncol(snpsGeno))
     corLong$colSample = rep(colnames(snpsGeno), each = ncol(snpsGeno))
     corLong = corLong[!is.na(corLong$cor),]
-#    return(corLong)
     sampleData<-read.csv(SampleDataPath)
     colnames(sampleData)<-paste0("row",colnames(sampleData))
     corLong<-dplyr::left_join(corLong,sampleData, by = c("rowSample" = paste0("row",ID_col)))
     colnames(sampleData)<-gsub("row","col",colnames(sampleData))
     corLong<-dplyr::left_join(corLong,sampleData, by = c("colSample" = paste0("col",ID_col)))
     return(corLong)
-#     corLong$rowBrain <- brain_sentrix_present$BrNum[brain_sentrix_match_row]
-#     corLong$colBrain <- brain_sentrix_present$BrNum[brain_sentrix_match_col]
-#     corLong$rowBatch <- brain_sentrix_present$Batch[brain_sentrix_match_row]
-#     corLong$colBatch <- brain_sentrix_present$Batch[brain_sentrix_match_col]
 }
