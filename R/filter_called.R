1d2a9bf7a0e640d8662664defdaf5e492884f023
Louise Huuki <lahuuki@gmail.com>
2021-03-29 15:20:07 -0400

    Add IRanges package def

--- R/filter_called.R
+++ R/filter_called.R
@@ -13,11 +13,12 @@
 #'
 #' @examples
 #' filter_called(genotyped)
+#' called_filter <- filter_called(called)
 filter_called <- function(called,DP_min=5,DP_max=80, ref_call=1,alt_call=1, overlaps=1, min_VDB=.1){
     called = called[VariantAnnotation::info(called)$DP > DP_min*ncol(called) &
             VariantAnnotation::info(called)$DP < DP_max*ncol(called) &
             Biostrings::nchar(VariantAnnotation::ref(called)) == ref_call &
-            elementNROWS(VariantAnnotation::alt(called)) == alt_call &
+            IRanges::elementNROWS(VariantAnnotation::alt(called)) == alt_call &
             GenomicRanges::countOverlaps(called, type = "equal") == overlaps &
             VariantAnnotation::info(called)$VDB >min_VDB,]
     return(called)
