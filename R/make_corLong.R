516ddcff953c9f6831a8634ca60211b770a1efba
Louise Huuki <lahuuki@gmail.com>
2021-04-26 15:22:41 -0400

    Update Documentation

--- R/make_corLong.R
+++ R/make_corLong.R
@@ -1,9 +1,14 @@
-#' Title
+#' make_corLong
 #'
-#' @param snpsGeno
-#' @param brainsentrix
+#' @param snps1 Table of snps, RNA or DNA,
+#' @param snps2 Table of snps, RNA or DNA, if NA snps1 will be compared to its self
+#' @param BrainTable1 Table containing ID info for samples in snps1
+#' @param BrainTable2 Table containing ID info for samples in snps2
+#' @param ID_col1 Name of column containing Samples names in BrainTable1
+#' @param ID_col2 Name of column containing Samples names in BrainTable2
 #'
-#' @return
+#' @return Data frame with correlation between samples in snps1 (or between snps1 and snps2)
+#' and ID info from BrainTables (at least BrNums for samples)
 #' @export
 #'
 #' @examples
@@ -15,18 +20,19 @@
 #' corLongtest<-make_corLong(snpsGeno,BrainTable1=brain_sentrix)
 #'
 #'corLongtest<-make_corlong
+#'
 make_corLong<- function(snps1,snps2=snps1,BrainTable1,BrainTable2=BrainTable1, ID_col1="ID",ID_col2=ID_col1){
     snpCor = cor(snps1,snps2, use="pairwise.comp")
     rownames(snpCor) = colnames(snps1)
     colnames(snpCor) = colnames(snps2)
-     snpCor[lower.tri(snpCor, diag=TRUE)] = NA
-     corLong = data.frame(cor = signif(as.numeric(snpCor),3))
-     corLong$row_sample = rep(rownames(snpCor), times = ncol(snpCor))
-     corLong$col_sample = rep(colnames(snpCor), each = nrow(snpCor))
-     corLong = corLong[!is.na(corLong$cor),]
-     BrainTable3<-BrainTable2
-     colnames(BrainTable1)<-paste0("row_",colnames(BrainTable1))
-     colnames(BrainTable2)<-paste0("col_",colnames(BrainTable3))
+    snpCor[lower.tri(snpCor, diag=TRUE)] = NA
+    corLong = data.frame(cor = signif(as.numeric(snpCor),3))
+    corLong$row_sample = rep(rownames(snpCor), times = ncol(snpCor))
+    corLong$col_sample = rep(colnames(snpCor), each = nrow(snpCor))
+    corLong = corLong[!is.na(corLong$cor),]
+    BrainTable3<-BrainTable2
+    colnames(BrainTable1)<-paste0("row_",colnames(BrainTable1))
+    colnames(BrainTable2)<-paste0("col_",colnames(BrainTable3))
     corLong<-dplyr::left_join(corLong,BrainTable1, by = c("row_sample" = paste0("row_",ID_col1)))
     corLong<-dplyr::left_join(corLong,BrainTable2, by = c("col_sample" = paste0("col_",ID_col2)))
     return(corLong)
