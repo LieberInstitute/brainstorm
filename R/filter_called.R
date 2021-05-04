22c403ffde257f80d5f17d98f8df4a7ee1d03419
Louise Huuki <lahuuki@gmail.com>
2021-05-04 10:48:01 -0400

    Update example

--- R/filter_called.R
+++ R/filter_called.R
@@ -12,13 +12,9 @@
 #' @export
 #'
 #' @examples
-#' geno_filtered <- filter_called(genotyped)
 #' dim(called)
-#'
-#' # 222 5568
 #' called_filter <- filter_called(called)
 #' dim(called_filter)
-#' # 748 5568
 filter_called <- function(called, DP_min = 5, DP_max = 80, ref_call = 1, alt_call = 1, overlaps = 1, min_VDB = .1) {
     called <- called[VariantAnnotation::info(called)$DP > DP_min * ncol(called) &
         VariantAnnotation::info(called)$DP < DP_max * ncol(called) &
