f662ef5459e802b5994bdebc95666e264bfaca3d
Louise Huuki <lahuuki@gmail.com>
2021-05-10 14:15:08 -0400

    Update tests and Messages

--- tests/testthat/test-make_corLong.R
+++ tests/testthat/test-make_corLong.R
@@ -1,4 +1,5 @@
-test_that("ncol corLong(test_snps)", {
+test_that("ncol & nrow corLong(test_snps)", {
     cor_test <- make_corLong(snps1 = test_snps, BrainTable1 = test_BrTable, ID_col1 = "Sample")
     expect_equal(ncol(cor_test), 5)
+    expect_equal(nrow(cor_test), 6)
 })
