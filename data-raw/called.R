8fa11f6c4f68a6e84585fe23f332102094884e2f
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-05-20 18:44:03 -0400

    Tried adding JHPCE paths to files. Missing pd_swap.csv

--- data-raw/called.R
+++ data-raw/called.R
@@ -2,13 +2,19 @@
 library(VariantAnnotation)
 library(jaffelab)
 
+## Originally at:
+# /dcl01/ajaffe/data/lab/brain_swap/RNAseq_Collection_postQC_n5568_10dataset_2020-07-22_hg38.vcf.gz
 called <- VariantAnnotation::readVcf("Data/RNAseq_Collection_postQC_n5568_10dataset_2020-07-22_hg38.vcf.gz",
     genome = "hg38"
 )
 
+## Originally at:
+# /dcl01/ajaffe/data/lab/brain_swap/common_missense_SNVs_hg38.bed
 snpMap_hg38 <- import("Data/common_missense_SNVs_hg38.bed")
 snpMap_hg38$chrpos <- paste0(seqnames(snpMap_hg38), ":", start(snpMap_hg38))
 
+## Originally at:
+# TODO
 pd <- read.csv("Data/pd_swap.csv")
 
 colData(called)$SampleID <- sapply(strsplit(colnames(called), "/"), function(x) x[length(x)])
