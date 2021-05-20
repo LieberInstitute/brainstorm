be4a5d407e7851b7c4069ed5b9d6f7ed7f666074
Louise Huuki <lahuuki@gmail.com>
2021-05-20 15:22:34 -0400

    Add brainstorm::test_snps to test

--- tests/testthat/test-check_cols.R
+++ tests/testthat/test-check_cols.R
@@ -1,9 +1,9 @@
 test_that("Cols present", {
-    expect_null(.check_cols(table = test_snps, check_colnames = c("A")))
-    expect_null(.check_cols(table = test_snps, check_colnames = c("A", "B")))
+    expect_null(.check_cols(table = brainstorm::test_snps, check_colnames = c("A")))
+    expect_null(.check_cols(table = brainstorm::test_snps, check_colnames = c("A", "B")))
 })
 
 test_that("Cols Missing", {
-    expect_error(.check_cols(table = test_snps, check_colnames = c("X", )))
-    expect_error(.check_cols(table = test_snps, check_colnames = c("X", "Y")))
+    expect_error(.check_cols(table = brainstorm::test_snps, check_colnames = c("X", )))
+    expect_error(.check_cols(table = brainstorm::test_snps, check_colnames = c("X", "Y")))
 })
