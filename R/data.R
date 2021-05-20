9a576806dc4f2816e32db766b473affad42ca148
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-05-20 19:04:42 -0400

    Start documenting example data

---
+++ R/data.R
@@ -0,0 +1,33 @@
+#' TODO genotyped
+#'
+#' TODO Description.
+#'
+#' @format A CollapsedVCF object.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/genotyped.R>
+"genotyped"
+
+#' TODO called
+#'
+#' TODO Description.
+#'
+#' @format TODO.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/called.R
+#"called" ## TODO: uncomment this line once data/called.rda is added to the repo
+
+#' Test SNP matrix
+#'
+#' A test SNP input `matrix`.
+#'
+#' @format A `matrix` with zeros and ones representing the genotypes.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/test_snps.R>
+"test_snps"
+
+#' Test brain donor table
+#'
+#' A test `data.frame` with example data for samples with brain donor ids (BrNums).
+#'
+#' @format A `data.frame` with two columns: `Sample` and `BrNum`.
+#' @source <https://github.com/LieberInstitute/brainstorm/blob/master/data-raw/test_BrTable.R>
+"test_BrTable"
+
+
