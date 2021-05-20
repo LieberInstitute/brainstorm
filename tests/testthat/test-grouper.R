5248c8d73ca1b2dd9b3a31eb85ca36def7f4c3b7
Louise Huuki <lahuuki@gmail.com>
2021-05-20 16:10:26 -0400

    Remove brainstorm::

--- tests/testthat/test-grouper.R
+++ tests/testthat/test-grouper.R
@@ -1,5 +1,5 @@
 test_that("Number of groups in cor_test", {
-    cor_test <- make_corLong(snps1 = brainstorm::test_snps, BrainTable1 = brainstorm::test_BrTable, ID_col1 = "Sample")
+    cor_test <- make_corLong(snps1 = test_snps, BrainTable1 = test_BrTable, ID_col1 = "Sample")
     g <- grouper(cor_test)
     expect_equal(length(g), 2)
 })
--- tests/testthat/test-make_corLong.R
+++ tests/testthat/test-make_corLong.R
@@ -1,5 +1,5 @@
 test_that("ncol & nrow corLong(test_snps)", {
-    cor_test <- make_corLong(snps1 = brainstorm::test_snps, BrainTable1 = brainstorm::test_BrTable, ID_col1 = "Sample")
+    cor_test <- make_corLong(snps1 = test_snps, BrainTable1 = test_BrTable, ID_col1 = "Sample")
     expect_equal(ncol(cor_test), 5)
     expect_equal(nrow(cor_test), 6)
 })
