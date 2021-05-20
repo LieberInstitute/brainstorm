c825762535ab3f34b5026f69e54c1f3465be0aa1
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-05-20 19:12:40 -0400

    Replace Data by data in scripts

--- data-raw/genotyped.R
+++ data-raw/genotyped.R
@@ -2,7 +2,7 @@
 
 ## Originally at:
 # /dcl01/ajaffe/data/lab/brain_swap/LIBD_Brain_merged_topmed_RNASeq_annotated_variants_043020.vcf
-genotyped <- VariantAnnotation::readVcf("Data/LIBD_Brain_merged_topmed_RNASeq_annotated_variants_043020.vcf",
+genotyped <- VariantAnnotation::readVcf("data/LIBD_Brain_merged_topmed_RNASeq_annotated_variants_043020.vcf",
     genome = "hg38"
 )
 usethis::use_data(genotyped, overwrite = TRUE)
